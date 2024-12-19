2c6c6f55707749021229c186124dd87a2b94d7aa
KoZZZtik-3d <kv.sobolev06@gmail.com>
2024-12-19 11:44:00 +0300

    feat: в FilmController добавлена обработка для запросов из задания (также убран List<Film> films, тк теперь storage)

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,49 +1,61 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.config.Config;
-import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
 
-import java.util.List;
-
-import static ru.yandex.practicum.filmorate.validator.Validator.validateFilm;
+import java.util.Collection;
+import java.util.Optional;
 
 @Slf4j
 @RestController
 @RequestMapping("/films")
 public class FilmController {
 
-    List<Film> films = Config.getDefaultFilms();
+    FilmService filmService;
+
+    @Autowired
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
+    }
+
+
 
     @PostMapping
-    public Film addFilm(@RequestBody Film film) {
-        validateFilm(film);
-        films.add(film);
-        log.info("Film added: {} {}", film.getName(), film.getReleaseDate().toString());
-        return film;
+    public Film createFilm(@RequestBody Film film) {
+       return filmService.createFilm(film);
     }
 
     @PutMapping
     public Film updateFilm(@RequestBody Film newFilm) {
-        validateFilm(newFilm);
-        for (int i = 0; i < films.size(); i++) {
-            var oldFilm = films.get(i);
-            if (oldFilm.getId() == newFilm.getId()) {
-                films.remove(i); // remove oldFilm
-                films.add(newFilm);
-                log.info("Film updated: ({} {}) -> ({} {})", oldFilm.getName(), oldFilm.getReleaseDate().toString(),
-                        newFilm.getName(), newFilm.getReleaseDate().toString());
-                return oldFilm;
-            }
-        }
-        throw new FilmNotFoundException();
+        return filmService.update(newFilm);
     }
 
     @GetMapping
-    public List<Film> getAllFilms() {
-        return films;
+    public Collection<Film> getAllFilms() {
+        return filmService.getAllFilms();
+    }
+
+    @GetMapping("/{id}")
+    public Film getById(@PathVariable("id") final int id) {
+        return filmService.getById(id);
+    }
+
+    @PutMapping("/{id}/like/{userId}")
+    public Film addLike(@PathVariable("id") final int id, @PathVariable("userId") final int userId) {
+        return  filmService.addLike(id, userId);
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public Film deleteLike(@PathVariable("id") final int id, @PathVariable("userId") final int userId) {
+        return  filmService.deleteLike(id, userId);
+    }
+
+    @GetMapping("/popular")
+    public Collection<Film> getPopularFilms(@RequestParam(value = "count", required = false) final int count) {
+        return filmService.getPopularFilms(Optional.ofNullable(count));
     }
 
 }
