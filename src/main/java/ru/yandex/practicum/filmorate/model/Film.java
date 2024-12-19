d2074b94f06c450166cdce0ea9e041d72386e997
KoZZZtik-3d <kv.sobolev06@gmail.com>
2024-12-19 11:39:09 +0300

    feat: создан abstract idObject для Film и User (хотя лучше, наверное, заменить на интерфейс)

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,17 +1,20 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.util.Set;
 
 @Data
-public class Film {
+public class Film extends idObject {
 
-    private int id;
     private String name;
     private String description;
     private LocalDate releaseDate;
     private Duration duration;
+    @JsonIgnore
+    private Set<Integer> likes;
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,14 +3,16 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
-public class User {
+public class User extends idObject {
 
-    private int id;
     private String email;
     private String login;
     private String name;
     private LocalDate birthday;
+    private final Set<Integer> friends = new HashSet<>();
 
 }
