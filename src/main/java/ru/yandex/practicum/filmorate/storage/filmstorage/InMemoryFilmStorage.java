e7611a1efd7a1f4943ef1c40ee98ddc301fdb41e
KoZZZtik-3d <kv.sobolev06@gmail.com>
2024-12-19 11:40:19 +0300

    feat: созданы InMemoryUserStorage и InMemoryFilmStorage (также был пофикшено наследование в этих классах)

--- src/main/java/ru/yandex/practicum/filmorate/storage/filmstorage/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/filmstorage/InMemoryFilmStorage.java
@@ -5,36 +5,17 @@ import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.InMemoryStorage;
 
+import javax.management.openmbean.KeyAlreadyExistsException;
+import java.util.Collection;
+import java.util.HashMap;
+
 @Component
-public class InMemoryFilmStorage extends InMemoryStorage<Film> {
+public class InMemoryFilmStorage extends InMemoryStorage<Film> implements FilmStorage {
 
-    @Override
-    public void add(Film film) {
-        super.list.add(film);
+    public InMemoryFilmStorage() {
+        map = new HashMap<>();
     }
 
-    @Override
-    public Film remove(int id) {
-        for (int i = 0; i < super.list.size(); i++) {
-            var curFilm = super.list.get(i);
-            if (curFilm.getId() == id) {
-                return super.list.remove(i);
-            }
-        }
-        throw new FilmNotFoundException();
-    }
 
-    @Override
-    public Film update(int id, Film film) {
-        for (int i = 0; i < super.list.size(); i++) {
-            var curFilm = super.list.get(i);
-            if (curFilm.getId() == id) {
-                super.list.remove(i);
-                super.list.add(film);
-                return curFilm;
-            }
-        }
-        throw new FilmNotFoundException();
-    }
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/userstorage/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/userstorage/InMemoryUserStorage.java
@@ -5,36 +5,55 @@ import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.InMemoryStorage;
 
+import javax.management.openmbean.KeyAlreadyExistsException;
+import java.util.Collection;
+import java.util.HashMap;
+
 @Component
-public class InMemoryUserStorage extends InMemoryStorage<User> {
+public class InMemoryUserStorage extends InMemoryStorage<User> implements UserStorage {
 
-    @Override
-    public void add(User film) {
-        super.list.add(film);
+    private int maxId = 0;
+
+    public InMemoryUserStorage() {
+        map = new HashMap<>();
     }
 
-    @Override
-    public User remove(int id) {
-        for (int i = 0; i < super.list.size(); i++) {
-            var curUser = super.list.get(i);
-            if (curUser.getId() == id) {
-                return super.list.remove(i);
-            }
-        }
-        throw new UserNotFoundException();
-    }
+//    @Override
+//    public boolean create(User obj) {
+//        if (obj.getId() == null) {
+//            obj.setId(currentId++);
+//        }
+//        if (map.containsKey(obj.getId())) {
+//            throw new KeyAlreadyExistsException();
+//        }
+//        map.put(obj.getId(), obj);
+//        return true;
+//    }
+//
+//    @Override
+//    public User remove(int id) {
+//        return map.remove(id);
+//    }
+//
+//    @Override
+//    public User update(User user) {
+//        if (!map.containsKey(user.getId())) {
+//            throw new UserNotFoundException();
+//        }
+//        return map.put(user.getId(), user);
+//    }
+//
+//    @Override
+//    public Collection<User> getAll() {
+//        return map.values();
+//    }
+//
+//
+//    @Override
+//    public User get(int id) {
+//        return map.get(id);
+//    }
 
-    @Override
-    public User update(int id, User film) {
-        for (int i = 0; i < super.list.size(); i++) {
-            var curUser = super.list.get(i);
-            if (curUser.getId() == id) {
-                super.list.remove(i);
-                super.list.add(film);
-                return curUser;
-            }
-        }
-        throw new UserNotFoundException();
-    }
+
 
 }
